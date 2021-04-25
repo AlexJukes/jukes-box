acc0d51dc9e7c0c450068ea8355ebedce2261103
Alex <alexanderjukes@gmail.com>
2021-04-25 17:19:38 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+	let name = 'world';
+	let codeWord = '';
+	let isTech = codeWord.toLowerCase()  ===  'tech'
+	console.log('code', codeWord)
+</script>
+
+<title>Alex Jukes' Site</title>
+
+<h1>Welcome</h1>
+
+
+
+<h2>
+	I'm Alex
+</h2>
+
+<h3>
+	How do you know me?
+</h3>
+
+<input type="text" bind:value={codeWord}/>
+
+<h3>
+	About
+</h3>
+<ul>
+	<li>Human</li>
+	{#if codeWord.toLowerCase()  ===  'tech'}
+	<li>Software Engineer</li>
+	{/if}
+</ul>
