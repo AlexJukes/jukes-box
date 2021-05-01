a7202e02c6c20adf2581002b6c7fc0e89dc7ba97
Alex <alexanderjukes@gmail.com>
2021-05-01 20:24:50 +0100

    Add favicon and logo with srcsets

--- src/App.svelte
+++ src/App.svelte
@@ -1,56 +1,54 @@
 <script>
-	let codeWord = '';
-	let hintToggle  = false;
-	function handleHintClick() {
-		hintToggle = !hintToggle;
-	}
-	import Cat from './Cat.svelte'
+  let codeWord = "";
+  let hintToggle = false;
+  function handleHintClick() {
+    hintToggle = !hintToggle;
+  }
+  import Cat from "./components/Cat.svelte";
+  import Logo from "./components/Logo.svelte";
 </script>
+
+<Logo />
 
 <h1>Welcome</h1>
 
+<h2>I'm Alex</h2>
 
-
-<h2>
-	I'm Alex
-</h2>
-
-<h3>
-	About
-</h3>
+<h3>About</h3>
 <ul>
-	<li>Human</li>
-	{#if codeWord.toLowerCase()  ===  'tech'}
-		<li><a href="https://github.com/AlexJukes" target="_blank" rel="noopener noreferrer">Software Engineer</a></li>
-	{/if}
-	{#if codeWord.toLowerCase()  ===  'cat'}
-		<Cat image={'bed'} />
-	{/if}
-	{#if codeWord.toLowerCase()  ===  'lobus'}
-		<Cat image={'lobus'} />
-	{/if}
+  <li>Human</li>
+  {#if codeWord.toLowerCase() === "tech"}
+    <li>
+      <a
+        href="https://github.com/AlexJukes"
+        target="_blank"
+        rel="noopener noreferrer">Software Engineer</a
+      >
+    </li>
+  {/if}
+  {#if codeWord.toLowerCase() === "cat"}
+    <Cat image={"bed"} />
+  {/if}
+  {#if codeWord.toLowerCase() === "lobus"}
+    <Cat image={"lobus"} />
+  {/if}
 </ul>
 
-
-<h3>
-	Want to know more?
-</h3>
+<h3>Want to know more?</h3>
 
 <h4>
-	Just say the word...
-	<input type="text" bind:value={codeWord}/>
+  Just say the word...
+  <input type="text" bind:value={codeWord} />
 </h4>
 
-<h5>
-	Want a hint?
-</h5>
+<h5>Want a hint?</h5>
 
-<button on:click={handleHintClick}>{hintToggle  ? 'No' : 'Yes'}</button>
+<button on:click={handleHintClick}>{hintToggle ? "No" : "Yes"}</button>
 
 {#if hintToggle}
-<p>Try...</p>	
- <ul>
-	 <li><span>cat</span></li>
-	 <li><span>tech</span></li>
- </ul>
+  <p>Try...</p>
+  <ul>
+    <li><span>cat</span></li>
+    <li><span>tech</span></li>
+  </ul>
 {/if}
---
+++ src/components/Logo.svelte
@@ -0,0 +1,25 @@
+<div class="logo-wrapper">
+  <a href="https://alexjukes.com">
+    <img
+      class="homepage-logo"
+      srcset="/logos/signature-logo-black_600.png 600w,
+             /logos/signature-logo-black_400.png 400w,
+             /logos/signature-logo-black_200.png 200w"
+      sizes="(max-width: 600px) 300px,
+            (max-width: 900px) 200px,
+             250px"
+      src="/logos/signature-logo-black_600.png"
+      alt="Alex Jukes' homepage"
+    />
+  </a>
+</div>
+
+<style>
+  .homepage-logo {
+    /* max-width: 200px; */
+  }
+
+  .logo-wrapper {
+    display: flex;
+  }
+</style>
