d8d1e2353e0f284dadda15c4ba0e7787325c73f3
Alex <alexanderjukes@gmail.com>
2021-05-02 09:40:44 +0100

    Add signature SVG, make homepage a grid

--- src/App.svelte
+++ src/App.svelte
@@ -8,47 +8,97 @@
   import Logo from "./components/Logo.svelte";
 </script>
 
-<Logo />
+<div class="homepage">
+  <div class="homepage__header">
+    <Logo />
+  </div>
 
-<h1>Welcome</h1>
+  <div class="homepage__main">
+    <h1>Welcome</h1>
 
-<h2>I'm Alex</h2>
+    <h2>I'm Alex</h2>
 
-<h3>About</h3>
-<ul>
-  <li>Human</li>
-  {#if codeWord.toLowerCase() === "tech"}
-    <li>
-      <a
-        href="https://github.com/AlexJukes"
-        target="_blank"
-        rel="noopener noreferrer">Software Engineer</a
-      >
-    </li>
-  {/if}
-  {#if codeWord.toLowerCase() === "cat"}
-    <Cat image={"bed"} />
-  {/if}
-  {#if codeWord.toLowerCase() === "lobus"}
-    <Cat image={"lobus"} />
-  {/if}
-</ul>
+    <h3>About</h3>
+    <ul>
+      <li>Human</li>
+      {#if codeWord.toLowerCase() === "tech"}
+        <li>
+          <a
+            href="https://github.com/AlexJukes"
+            target="_blank"
+            rel="noopener noreferrer">Software Engineer</a
+          >
+        </li>
+      {/if}
+      {#if codeWord.toLowerCase() === "cat"}
+        <Cat image={"bed"} />
+      {/if}
+      {#if codeWord.toLowerCase() === "lobus"}
+        <Cat image={"lobus"} />
+      {/if}
+    </ul>
+  </div>
 
-<h3>Want to know more?</h3>
+  <div class="homepage__code-input-text">
+    <h3>Want to know more?</h3>
 
-<h4>
-  Just say the word...
-  <input type="text" bind:value={codeWord} />
-</h4>
+    <label for="code-input">
+      <h4>Just type the word...</h4>
+    </label>
+  </div>
 
-<h5>Want a hint?</h5>
+  <div class="homepage__code-input">
+    <input type="text" id="code-input" bind:value={codeWord} />
+  </div>
 
-<button on:click={handleHintClick}>{hintToggle ? "No" : "Yes"}</button>
+  <div class="homepage__hint">
+    <h5>Want a hint?</h5>
 
-{#if hintToggle}
-  <p>Try...</p>
-  <ul>
-    <li><span>cat</span></li>
-    <li><span>tech</span></li>
-  </ul>
-{/if}
+    <button on:click={handleHintClick}>{hintToggle ? "No" : "Yes"}</button>
+
+    {#if hintToggle}
+      <p>Try...</p>
+      <ul>
+        <li><span>cat</span></li>
+        <li><span>tech</span></li>
+      </ul>
+    {/if}
+  </div>
+</div>
+
+<style>
+  .homepage {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: auto;
+
+    grid-template-areas:
+      "header . ."
+      ". main ."
+      ". code-input-text ."
+      "code-input code-input code-input"
+      ". hint ."
+      "footer footer footer";
+  }
+
+  .homepage__header {
+    grid-area: header;
+  }
+
+  .homepage__main {
+    grid-area: main;
+  }
+
+  .homepage__code-input-text {
+    grid-area: code-input-text;
+  }
+
+  .homepage__code-input {
+    grid-area: code-input;
+    justify-self: center;
+  }
+
+  .homepage__hint {
+    grid-area: hint;
+  }
+</style>
--- src/components/Logo.svelte
+++ src/components/Logo.svelte
@@ -2,13 +2,7 @@
   <a href="https://alexjukes.com">
     <img
       class="homepage-logo"
-      srcset="/logos/signature-logo-black_600.png 600w,
-             /logos/signature-logo-black_400.png 400w,
-             /logos/signature-logo-black_200.png 200w"
-      sizes="(max-width: 600px) 300px,
-            (max-width: 900px) 200px,
-             250px"
-      src="/logos/signature-logo-black_600.png"
+      src="/logos/signature-black.svg"
       alt="Alex Jukes' homepage"
     />
   </a>
@@ -16,7 +10,7 @@
 
 <style>
   .homepage-logo {
-    /* max-width: 200px; */
+    max-width: 250px;
   }
 
   .logo-wrapper {
