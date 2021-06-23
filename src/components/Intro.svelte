4852e2f8d2d7a315be7d91115e768e84dc4d9826
Alex <alexanderjukes@gmail.com>
2021-06-23 20:16:51 +0100

    Update animations and add pointer chevron

--- src/components/Intro.svelte
+++ src/components/Intro.svelte
@@ -2,6 +2,10 @@
 
 <h2 class="intro-text">I'm Alex</h2>
 
+<div class="pointer-wrapper">
+  <div class="chevron-pointer">></div>
+</div>
+
 <style>
   .welcome-text {
     font-size: 9em;
@@ -13,6 +17,33 @@
     font-size: 4em;
     font-weight: normal;
     animation: fadein 4s;
+    animation-delay: 1.5s;
+    opacity: 0;
+    animation-fill-mode: forwards;
+  }
+
+  .pointer-wrapper {
+    font-size: 2em;
+    animation: floating 3s ease-in-out 1.5s infinite, fadein 3s;
+    animation-delay: 1.5s;
+    opacity: 0;
+    animation-fill-mode: forwards;
+  }
+
+  .chevron-pointer {
+    transform: rotate(90deg);
+  }
+
+  @keyframes floating {
+    0% {
+      transform: translate(0, 0px);
+    }
+    50% {
+      transform: translate(0, 15px);
+    }
+    100% {
+      transform: translate(0, -0px);
+    }
   }
 
   @keyframes fadein {
